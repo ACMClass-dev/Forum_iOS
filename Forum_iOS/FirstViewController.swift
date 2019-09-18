047f054a5a062b48526c0e05276b2e644c4afb04
oscardhc <oscardhc@sjtu.edu.cn>
2019-09-18 14:11:19 +0800

    Initial Commit

---
+++ Forum_iOS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Forum_iOS
+//
+//  Created by Haichen Dong on 2019/9/18.
+//  Copyright © 2019 Haichen Dong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Forum_iOSTests/Forum_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Forum_iOSTests.swift
+//  Forum_iOSTests
+//
+//  Created by Haichen Dong on 2019/9/18.
+//  Copyright © 2019 Haichen Dong. All rights reserved.
+//
+
+import XCTest
+@testable import Forum_iOS
+
+class Forum_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
